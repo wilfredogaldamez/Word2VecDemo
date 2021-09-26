e3895389c9aa799b13eabcca8105421927fac2bb
Wilfredo Galdamez <wilfredogaldamez1994@gmail.com>
2021-09-26 10:54:47 -0400

    added flask route

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import gensim.downloader as api
 from multiprocessing import cpu_count
 from gensim.models.word2vec import Word2Vec
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
  # load your model
@@ -13,14 +13,21 @@ w2v_model = Word2Vec.load('Word2VecModel')
 
 
 # similar words to the word "time"
-for word in w2v_model.wv.most_similar('trump'):
+rList = []
+for word in w2v_model.wv.most_similar('simulation'):
     print(word)
+    rList.append(word)
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hindex():
-    return render_template('index.html')
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        return render_template('index.html', rList = rList)
+    else:
+        return render_template('index.html', rList = rList)
 
 
 
